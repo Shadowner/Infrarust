8625ec563825dcb1b11251d847ad5bbb845faae7
Shadowner <12647984+Shadowner@users.noreply.github.com>
2026-04-05 20:17:50 +0200

    MINOR : Bumping to alpha 3

--- docs/v2/.vitepress/theme/constants.ts
+++ docs/v2/.vitepress/theme/constants.ts
@@ -1,4 +1,4 @@
 export const GITHUB_REPO = 'Shadowner/Infrarust'
 export const GITHUB_URL = 'https://github.com/Shadowner/Infrarust'
 export const DISCORD_URL = 'https://discord.gg/sqbJhZVSgG'
-export const VERSION = 'v2.0-alpha01'
+export const VERSION = '2.0.0-alpha.3'
